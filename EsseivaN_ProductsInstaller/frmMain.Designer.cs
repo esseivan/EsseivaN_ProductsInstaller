ded5a3f014d693486de440723e41e4486221a4f6
EsseivaN <nicolas.esseiva@gmail.com>
2019-03-13 19:29:22 +0100

    Initial upload

---
+++ EsseivaN_ProductsInstaller/frmMain.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace EsseivaN_ProductsInstaller
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.generalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editUrlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.installSelectedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.visitPublishPageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(9, 24);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(782, 417);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.generalToolStripMenuItem,
+            this.refreshToolStripMenuItem,
+            this.installSelectedToolStripMenuItem,
+            this.visitPublishPageToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(9, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(782, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // generalToolStripMenuItem
+            // 
+            this.generalToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editUrlToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.generalToolStripMenuItem.Name = "generalToolStripMenuItem";
+            this.generalToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.generalToolStripMenuItem.Text = "General";
+            // 
+            // editUrlToolStripMenuItem
+            // 
+            this.editUrlToolStripMenuItem.Name = "editUrlToolStripMenuItem";
+            this.editUrlToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.editUrlToolStripMenuItem.Text = "Edit url";
+            this.editUrlToolStripMenuItem.Click += new System.EventHandler(this.editUrlToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F5;
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // installSelectedToolStripMenuItem
+            // 
+            this.installSelectedToolStripMenuItem.Name = "installSelectedToolStripMenuItem";
+            this.installSelectedToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.installSelectedToolStripMenuItem.Text = "Install selected";
+            this.installSelectedToolStripMenuItem.Click += new System.EventHandler(this.installSelectedToolStripMenuItem_Click);
+            // 
+            // visitPublishPageToolStripMenuItem
+            // 
+            this.visitPublishPageToolStripMenuItem.Name = "visitPublishPageToolStripMenuItem";
+            this.visitPublishPageToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
+            this.visitPublishPageToolStripMenuItem.Text = "Visit publish page";
+            this.visitPublishPageToolStripMenuItem.Click += new System.EventHandler(this.visitPublishPageToolStripMenuItem_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Padding = new System.Windows.Forms.Padding(9, 0, 9, 9);
+            this.Text = "EsseivaN Installer";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem generalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem installSelectedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editUrlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem visitPublishPageToolStripMenuItem;
+    }
+}
+
---
+++ EsseivaN_ProductsInstaller/frmMain.cs
@@ -0,0 +1,367 @@
+﻿using EsseivaN.Tools;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace EsseivaN_ProductsInstaller
+{
+    public partial class frmMain : Form
+    {
+        public class Data
+        {
+            public string Name { get; set; }
+            public string ProductName { get; set; }
+            public int Index { get; set; }
+            public string Url { get; set; }
+            public DataType Type { get; set; }
+            public string Version { get; set; }
+            public bool Installed { get; set; }
+            public string InstalledVersion { get; set; }
+            public string Description { get; set; }
+            public string Size { get; set; }
+            public string Date { get; set; }
+            public string InstallerUrl { get; set; }
+            public string PublishUrl { get; set; }
+
+            public Data()
+            {
+
+            }
+
+            public Data(DataType Type)
+            {
+                this.Type = Type;
+            }
+
+            public Data(string Name, string Url, int Index)
+            {
+                this.Name = Name;
+                this.Url = Url;
+                this.Index = Index;
+            }
+
+            public override string ToString()
+            {
+                return $"Data : [Name={Name} ; Url={Url} ; Index={Index}; Type={Type.ToString()}]";
+            }
+
+            public enum DataType
+            {
+                None = 0,
+                Unknown = 1,
+                App = 10,
+                Project = 20,
+            }
+        }
+        List<Data> datas = new List<Data>();
+        private int GroupAppIndex, GroupProjectIndex;
+        private string Url = @"http://www.esseivan.ch/files/infos.xml";
+
+        public frmMain()
+        {
+            InitializeComponent();
+            InitList();
+        }
+
+        private void InitList()
+        {
+            listView1.FullRowSelect = true;
+            listView1.CheckBoxes = true;
+            listView1.ShowGroups = true;
+            listView1.View = View.Details;
+            listView1.Groups.Clear();
+            GroupAppIndex = listView1.Groups.Add(new ListViewGroup("Apps"));
+            GroupProjectIndex = listView1.Groups.Add(new ListViewGroup("Projects"));
+
+            listView1.Columns.Clear();
+            listView1.Columns.Add("Name", 120);
+            listView1.Columns.Add("Description", 120);
+            listView1.Columns.Add("Version", 120);
+            listView1.Columns.Add("Size", 120);
+            listView1.Columns.Add("Installed version", 120);
+            listView1.Columns.Add("Infos", 120);
+        }
+
+        private void FillList()
+        {
+            if (datas == null)
+            {
+                return;
+            }
+
+            if (datas.Count == 0)
+            {
+                return;
+            }
+
+            listView1.Items.Clear();
+
+            int groupIndex = 0;
+            foreach (Data data in datas)
+            {
+                switch (data.Type)
+                {
+                    case Data.DataType.App:
+                        groupIndex = GroupAppIndex;
+                        break;
+                    case Data.DataType.Project:
+                        groupIndex = GroupProjectIndex;
+                        break;
+                    default:
+                        break;
+                }
+
+                ListViewItem t = new ListViewItem(new string[] {
+                    data.Name,
+                    data.Description,
+                    data.Version,
+                    data.Size,
+                    (data.Installed ? data.InstalledVersion : (data.Type == Data.DataType.Project ? "": "Not installed")),
+                    (data.Installed && Version.Parse(data.InstalledVersion) < Version.Parse(data.Version) ? "Update available":"")},
+                    listView1.Groups[groupIndex]);
+                listView1.Items.Add(t);
+            }
+        }
+
+        private Data generateData(XmlNodeList node, Data.DataType Type)
+        {
+            Data data_output = new Data(Type);
+            foreach (XmlNode item in node)
+            {
+                switch (item.Name)
+                {
+                    case "index":
+                        data_output.Index = int.Parse(item.InnerText);
+                        break;
+                    case "name":
+                        data_output.Name = item.InnerText;
+                        break;
+                    case "url":
+                        data_output.Url = item.InnerText;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            // Get extra info if available
+            if (Type == Data.DataType.App || Type == Data.DataType.Project)
+            {
+                // Read app's info file
+                XmlElement appInfo = null;
+                try
+                {
+                    appInfo = XmlWebReader.ReadFromWeb(data_output.Url).DocumentElement;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Infos not found for : " + data_output.Url);
+                }
+
+                if (appInfo == null)
+                {
+                    return data_output;
+                }
+
+                foreach (XmlNode item in appInfo)
+                {
+                    switch (item.Name)
+                    {
+                        case "productname":
+                            data_output.ProductName = item.InnerText;
+                            break;
+                        case "version":
+                            data_output.Version = item.InnerText;
+                            break;
+                        case "description":
+                            data_output.Description = item.InnerText;
+                            break;
+                        case "size":
+                            data_output.Size = item.InnerText;
+                            break;
+                        case "date":
+                            data_output.Date = item.InnerText;
+                            break;
+                        case "installer":
+                            data_output.InstallerUrl = item.InnerText;
+                            break;
+                        case "url":
+                            data_output.PublishUrl = item.InnerText;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return data_output;
+        }
+
+        public void UpdateInfos(string url)
+        {
+            try
+            {
+                // read from web
+                XmlDocument doc = XmlWebReader.ReadFromWeb(url);
+                if (doc == null)
+                {
+                    Dialog.ShowDialog(new Dialog.DialogConfig()
+                    {
+                        Message = "Unable to read info file : \n" + url,
+                        Title = "Error",
+                        Button1 = Dialog.ButtonType.OK,
+                        Icon = Dialog.DialogIcon.Error,
+                    });
+                    return;
+                }
+
+                datas.Clear();
+                listView1.Items.Clear();
+
+                // Get apps
+                XmlNodeList nodes = doc.GetElementsByTagName("app");
+                if (nodes.Count == 0)
+                {
+                    Console.WriteLine("Unable to retrieve apps");
+                }
+                else
+                {
+                    // For each app
+                    foreach (XmlElement app in nodes)
+                    {
+                        if (app.HasChildNodes)
+                        {
+                            datas.Add(generateData(app.ChildNodes, Data.DataType.App));
+                        }
+                    }
+                }
+
+                // Get projects
+                nodes = doc.GetElementsByTagName("project");
+                if (nodes.Count == 0)
+                {
+                    Console.WriteLine("Unable to retrieve projects");
+                }
+                else
+                {
+                    foreach (XmlElement app in nodes)
+                    {
+                        if (app.HasChildNodes)
+                        {
+                            datas.Add(generateData(app.ChildNodes, Data.DataType.Project));
+                        }
+                    }
+                }
+                UpdateFromRegistry();
+                FillList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex); Dialog.ShowDialog(new Dialog.DialogConfig()
+                {
+                    Message = "Unable to process info file : \n" + ex,
+                    Title = "Error",
+                    Button1 = Dialog.ButtonType.OK,
+                    Icon = Dialog.DialogIcon.Error,
+                });
+            }
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void editUrlToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var dr = Dialog.ShowDialog(new Dialog.DialogConfig()
+            {
+                Message = "Enter the info xml url",
+                Title = "Enter new url",
+                Button1 = Dialog.ButtonType.OK,
+                Button2 = Dialog.ButtonType.Cancel,
+                Input = true,
+                DefaultInput = Url
+            });
+
+            if (dr.DialogResult == Dialog.DialogResult.OK)
+            {
+                Url = dr.UserInput;
+                UpdateInfos(Url);
+            }
+        }
+
+        private void UpdateFromRegistry()
+        {
+            string Manufacturer = "EsseivaN";
+            using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Default))
+            using (var key = hklm.OpenSubKey($@"Software\{Manufacturer}\"))
+            {
+                if (key == null)
+                {
+                    return;
+                }
+
+                if (key.SubKeyCount == 0)
+                {
+                    return;
+                }
+
+                foreach (string item in key.GetSubKeyNames())
+                {
+                    var t = datas.Where(x => x.ProductName == item);
+                    if (t.Count() != 0)
+                    {
+                        t.FirstOrDefault().Installed = true;
+                        var t2 = key.OpenSubKey(item).GetValue("version");
+                        if (t2 != null)
+                        {
+                            t.FirstOrDefault().InstalledVersion = t2.ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UpdateInfos(Url);
+        }
+
+        private void installSelectedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Data selected;
+            foreach (int item in listView1.CheckedIndices)
+            {
+                selected = datas[item];
+                Console.WriteLine("Downloading and installing " + selected.Name);
+                if (selected.InstallerUrl != string.Empty && selected.InstallerUrl != null)
+                {
+                    MiscTools.DownloadFile(selected.InstallerUrl, true);
+                }
+            }
+        }
+
+        private void visitPublishPageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Data selected;
+            foreach (int item in listView1.SelectedIndices)
+            {
+                selected = datas[item];
+                if (selected.PublishUrl != string.Empty && selected.PublishUrl != null)
+                {
+                    Process.Start(selected.PublishUrl);
+                }
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            UpdateInfos(Url);
+        }
+    }
+}
